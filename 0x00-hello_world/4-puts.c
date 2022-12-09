deeb25012572de7c01629aa7a7848e862ae8b1ff
emmanuelsteven <write2stevemmy@gmail.com>
2022-12-08 16:02:12 -0800

    task 4 updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-/**
+/*
+ *
  * main - Entry point
+ *
  * Return: Always 0 (Success)
- * */
+ *
+ */
 
 int main(void)
 
@@ -11,4 +14,5 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 
 return (0)
+
 }
