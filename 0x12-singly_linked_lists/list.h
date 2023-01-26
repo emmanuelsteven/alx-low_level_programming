99432f7b9a99f6ac39ebe107bc4e7af3986ae2c0
emmanuelsteven <write2stevemmy@gmail.com>
2023-01-26 15:36:52 -0800

    singly task

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,25 +1,27 @@
-#include <stdio.h>
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
+
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: singly linked list
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Return: number of elements in the list.
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-size_t print_list(const list_t *h)
+typedef struct list_s
 {
-	size_t nelem;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
-	nelem = 0;
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		nelem++;
-	}
-	return (nelem);
-}
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
